c28df7983f0213ae8dec454658e565fd9437f0ab
Dhthakkar <thakkardhruv440@gmail.com>
2016-06-02 19:41:06 +0530

    Add files via upload

---
+++ php/registration/1st.php
@@ -0,0 +1,15 @@
+<?php
+
+$name = $_POST['uname'];
+$mail = $_POST['umail'];
+$dob = $_POST['dob'];
+$add = $_POST['texta'];
+
+
+echo "Your Name is:".$name."<br>";
+echo "Your Email is:".$mail."<br>";
+echo "Your DOB is:".$dob."<br>";
+echo "Your Address is:".$add."<br>";
+
+
+?>
---
+++ php/registration/index.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<form action="1st.php" method="post">
+<table>
+<tr>
+<td>Enter Name:</td><td><input type="text" name="uname"></td>
+</tr>
+<tr>
+<td>Enter Email:</td><td><input type="email" name="umail"></td>
+</tr>
+<tr>
+<td>Enter DOB:</td><td><input type="date" name="dob"></td>
+</tr>
+
+<tr>
+<td>Enter Address:</td><td><textarea name="texta"></textarea></td> 
+</tr>
+<tr>
+<td></td>
+<td><input type="submit" value="submit"></td>
+</tr>
+</table>
+
+
+</form>
+
+
+</body>
+</html>
